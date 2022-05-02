b58c715ec9b1868d03d1be63509ce4557b2b6550
Cataline Baert <snuf_kin@yahoo.com>
2022-05-02 11:10:23 +0200

    Add presence validation for overview of Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,5 @@
 
 class Movie < ApplicationRecord
   validates_presence_of :title
+  validates_presence_of :overview
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,9 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  describe "validations" do
+  describe 'validations' do
     subject(:movie) { create(:movie) }
 
     it { should validate_presence_of :title }
+    it { should validate_presence_of :overview }
   end
 end
