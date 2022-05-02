86c543ae6fa1b23c709c4ad643c42f7fe5819dee
Cataline Baert <snuf_kin@yahoo.com>
2022-05-02 11:09:12 +0200

    Add presence validation for title of Movie

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,5 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    subject(:movie) { create(:movie) }
+
+    it { should validate_presence_of :title }
+  end
 end
