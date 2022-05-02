572eb5f2d476bfc1f45acb5c3c16d678cf4b6d9d
Cataline Baert <snuf_kin@yahoo.com>
2022-05-02 11:34:31 +0200

    Add numericality validations for rating of Movie

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -13,5 +13,8 @@ RSpec.describe Movie, type: :model do
 
     it { should allow_value('https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg').for :poster_url }
     it { should_not allow_value('/kqjL17yufvn9OVLyXYpvtyrFfak.jpg').for :poster_url }
+
+    it { should validate_numericality_of(:rating).is_greater_than_or_equal_to 0.0 }
+    it { should validate_numericality_of(:rating).is_less_than_or_equal_to 10.0 }
   end
 end
