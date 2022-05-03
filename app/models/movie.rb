a53c7687ead7b793252b4736b3903a5087586cad
Cataline Baert <snuf_kin@yahoo.com>
2022-05-03 18:31:03 +0200

    Improve poster_url regex (brakeman)

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,7 +4,7 @@ class Movie < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :overview
   validates_uniqueness_of :title
-  validates_format_of :poster_url, with: %r{https://image.tmdb.org/t/p/\w+/\w+\.jpg}
+  validates_format_of :poster_url, with: %r{\Ahttps://image.tmdb.org/t/p/\w+/\w+\.jpg\z}
   validates_numericality_of :rating, greater_than_or_equal_to: 0.0
   validates_numericality_of :rating, less_than_or_equal_to: 10.0
 end
