6b0500eace6ac66bf371fa51e417ab22d3eeb9dd
Cataline Baert <snuf_kin@yahoo.com>
2022-05-04 15:59:32 +0200

    Add presence and uniqueness validations to name of List

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -3,5 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    subject(:list) { create(:list) }
+
+    it { should validate_presence_of :name }
+    it { should validate_uniqueness_of :name }
+  end
 end
