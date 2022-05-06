b03c3e6757fce01da6709f1aafb7b28c6803b386
Cataline Baert <snuf_kin@yahoo.com>
2022-05-06 21:27:10 +0200

    Change subject to subject bang

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
   describe 'validations' do
-    subject(:movie) { create(:movie) }
+    subject!(:movie) { create(:movie) }
 
     it { should validate_presence_of :title }
     it { should validate_presence_of :overview }
