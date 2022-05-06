f806125ce46300092dcb475a68be5f73f4c84519
Cataline Baert <snuf_kin@yahoo.com>
2022-05-06 23:41:28 +0200

    Implement new and create actions of lists controller with request specs

--- spec/requests/lists_controller_spec.rb
+++ spec/requests/lists_controller_spec.rb
@@ -4,12 +4,14 @@ require 'rails_helper'
 
 RSpec.describe 'ListsController', type: :request do
   describe 'GET /lists' do
-    let!(:list) { create :list }
+    let!(:lists) { [create(:list), create(:list)] }
 
-    it 'shows a list of lists' do
+    it 'shows all the lists' do
       get root_url
+
       expect(response).to have_http_status(:ok)
-      expect(response.body).to include list.name
+      expect(response.body).to include lists[0].name
+      expect(response.body).to include lists[1].name
     end
   end
 
@@ -18,8 +20,40 @@ RSpec.describe 'ListsController', type: :request do
 
     it 'shows the details of a list' do
       get list_url(list)
+
       expect(response).to have_http_status(:ok)
       expect(response.body).to include list.name
+    end
+  end
+
+  describe 'GET /lists/new' do
+    it 'shows the page to create a new list' do
+      get new_list_url
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include 'Add a new list'
+    end
+  end
+
+  describe 'POST /lists' do
+    let!(:valid_attributes) { attributes_for(:list) }
+    let!(:invalid_attributes) { attributes_for(:list, name: nil, image: nil) }
+
+    context 'with valid parameters' do
+      it 'creates a new list successfully' do
+        expect { post lists_url, params: { list: valid_attributes } }.to change(List, :count).by(1)
+        expect(response).to redirect_to(list_url(List.last))
+        follow_redirect!
+        expect(response.body).to include List.last.name
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not create a new list' do
+        expect { post lists_url, params: { list: invalid_attributes } }.to change(List, :count).by(0)
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to include 'Add a new list'
+      end
     end
   end
 end
