0265b294e34ac9edc302b18fec5568ecaac8d0b3
Cataline Baert <snuf_kin@yahoo.com>
2022-05-07 01:38:52 +0200

    Start to implement new list creation with invalid attributes

--- spec/system/lists/create_spec.rb
+++ spec/system/lists/create_spec.rb
@@ -21,4 +21,20 @@ RSpec.describe 'Create list', type: :system do
     expect(page).to have_current_path(list_path(List.last))
     assert_selector 'h1', text: list.name
   end
+
+  scenario 'with invalid name and image' do
+    list = create(:list)
+    list_nb = List.count
+
+    visit root_path
+    click_link 'Create List'
+    expect(page).to have_current_path(new_list_path)
+
+    fill_in 'Name', with: list.name
+    click_button 'Create List'
+
+    expect(page).to have_content('Title has already been taken')
+    expect(page).to have_content("Image can't be blank")
+    expect(List.count).to eq(list_nb)
+  end
 end
