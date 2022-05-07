5ad3b938ab3280d29ae11db6283f2b8b9fc7d888
Cataline Baert <snuf_kin@yahoo.com>
2022-05-07 19:12:34 +0200

    Add bookmark associations to Movie and List models

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
   has_one_attached :image
 
   validates_presence_of :name
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -3,13 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
+  describe 'associations' do
+    subject!(:list) { create(:list) }
+
+    it { should have_many(:bookmarks).dependent(:destroy) }
+    it { should have_many(:movies).through(:bookmarks) }
+    it { should have_one_attached(:image) }
+  end
+
   describe 'validations' do
     subject!(:list) { create(:list) }
 
     it { should validate_presence_of :name }
     it { should validate_uniqueness_of :name }
-
-    it { should have_one_attached(:image) }
     it { should validate_presence_of :image }
   end
 end
